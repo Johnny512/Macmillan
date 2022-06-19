472042faee9c9583b536999ca32096f438aa482a
Johnny <jcastillo02@live.com>
2022-06-19 15:24:37 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, render_template, jsonify, make_response
+import json
+
+app = Flask(__name__)
+
+@app.route("/sreapi/v1/users", methods=['GET', 'POST'])
+def main():
+    if request.method == 'GET':
+        return get_users()
+    else:
+        return add_user()
+
+def get_users():
+    with open('openapi.json', 'r') as file:
+        data = json.loads(file.read())
+        # Returns the list of users
+        users = data['components']['schemas']['sreapi_v1_userlist']['example']['users']
+        response = make_response(
+            jsonify(
+                users
+            )
+        )
+        response.headers["Content-Type"] = "application/json"
+    return response
+
+def add_user():
+    with open('openapi.json', 'r+') as file:
+        data = json.load(file)
+        user = data['components']['schemas']['sreapi_v1_user']['example']
+        data['components']['schemas']['sreapi_v1_userlist']['example']['users'].append(user)
+        data['components']['schemas']['sreapi_v1_userlist']['example']['count'] += 1
+        file.seek(0)
+        # Save changes to file
+        json.dump(data, file, indent=4)
+        response = make_response(
+            jsonify(
+                data['components']['schemas']['sreapi_v1_userlist']['example']
+            )
+        )
+        response.headers["Content-Type"] = "application/json"
+    return response
+
+@app.route('/')
+def get_root():
+    print('sending root')
+    return render_template('index.html')
+
+@app.route('/api/docs')
+def get_docs():
+    print('sending docs')
+    return render_template('swaggerui.html')
---
+++ sre_homework.md
@@ -0,0 +1,22 @@
+# SRE Developer Interview Homework
+
+
+## Programming Assignment
+
+Create a flask based application that implements the following OPENAPI/Swagger-3 REST interfaces
+
+* Create a docker container of your work
+* Provide the following for the follow up interview
+  * dockerfile
+  * Implementation python code
+
+You will be asked to demonstrate the code in the follow up.
+
+## Diversity Awareness Statements
+
+What does Diversity/Equity and Incusiveness mean to you?
+  _It means that whoever you work with, you should treat them how you want to be treated_
+
+
+Macmillan has made a commitment to diversity & inclusion in many areas of our work. How could you envision making this commitment evident in your job?
+  _I would make sure that I make an effort to understand my co-worker's cultures and beliefs and treat them with respect_
